03e1fedb4b5fefe3d6d6252c0e505f2fd68b4457
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 12:36:34 -0600

    Add Learner Model

---
+++ BeeStudy/Models/Learner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Models
+{
+    public class Learner
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Is Registered User")]
+        public bool IsRegisteredUser { get; set; }
+
+        //Relationships
+        public List<Registration> Registrations { get; set; } = new List<Registration>();
+
+    }
+}
