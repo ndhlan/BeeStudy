c028a3046069b24f216a8ea4b3124631c1a81374
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 13:16:32 -0600

    Add RegistrationService

---
+++ BeeStudy/Data/Services/RegistrationService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Data.Services
+{
+    public class RegistrationService: IRegistrationService
+    {
+        private readonly ApplicationDbContext _context;
+        private CourseService _courseService;
+        private LearnerService _learnerService;
+
+        public RegistrationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICourseService CourseService
+        {
+            get
+            {
+                return _courseService = _courseService ?? new CourseService(_context);
+            }
+            set
+            {
+
+            }
+        }
+
+        public ILearnerService LearnerService
+        {
+            get
+            {
+                return _learnerService = _learnerService ?? new LearnerService(_context);
+            }
+            set
+            {
+
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+
+    }
+}
