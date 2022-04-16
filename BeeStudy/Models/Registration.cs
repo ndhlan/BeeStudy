36146be1ca65b2897f52d59571ec48ef78d1ef45
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 12:40:35 -0600

    Add Registration Model and RegistrationViewModel

---
+++ BeeStudy/Models/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Models
+{
+    public class Registration
+    {
+        public int CourseId { get; set; }
+        public int LearnerId { get; set; }
+
+        public Course Course { get; set; }
+        public Learner Learner { get; set; }
+
+
+    }
+}
---
+++ BeeStudy/Models/RegistrationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Models
+{
+    public class RegistrationViewModel
+    {
+        public Course Course { get; set; }
+
+        public Learner Learner { get; set; }
+
+    }
+}
