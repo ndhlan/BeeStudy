a2d2be8ecba344b8fc73cd0642ed5e312e62ab52
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 12:57:34 -0600

    Add ILearnerService Interface

---
+++ BeeStudy/Data/Services/ILearnerService.cs
@@ -0,0 +1,28 @@
+﻿using BeeStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Data.Services
+{
+    public interface ILearnerService
+    {
+        Task<IEnumerable<Learner>> GetAllAsync();
+
+        Task<Learner> GetByIdAsync(int id);
+
+        Task<Learner> GetByEmailAsync(string email);
+
+        Task<IEnumerable<Learner>> GetAllByCourseAsync(int courseId);
+
+        Task AddAsync(Learner newLearner);
+
+        Task<Learner> UpdateAsync(int id, Learner learner);
+
+        Task DeleteAsync(int id);
+
+        Task RemoveCourseAsync(int learnerId, int courseId);
+
+    }
+}
