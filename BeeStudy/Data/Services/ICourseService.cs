20cfa1a685321fa4ff2c91fc8b31e2f61282071a
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 12:56:02 -0600

    Add ICourseService Interface

---
+++ BeeStudy/Data/Services/ICourseService.cs
@@ -0,0 +1,30 @@
+﻿using BeeStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Data.Services
+{
+    interface ICourseService
+    {
+        Task<IEnumerable<Course>> GetAllAsync();
+
+        Task<Course> GetByIdAsync(int id);
+
+        Task<Course> GetByUdemyIdAsync(int UdemyId);
+
+        Task<Course> GetDetailsByUrlAsync(string url);
+
+        Task<IEnumerable<Course>> GetAllByLearnerAsync(int learnerId);
+
+        Task AddAsync(Course newCourse);
+
+        Task<Course> UpdateAsync(int id, Course newCourse);
+
+        Task DeleteAsync(int id);
+
+        Task RemoveLearnerAsync(int courseId, int learnerId);
+
+    }
+}
