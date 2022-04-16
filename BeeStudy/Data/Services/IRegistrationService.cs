1f403a302470f98f5bba71182450060fc70b0c43
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 13:10:47 -0600

    Add IRegistrationService interface

---
+++ BeeStudy/Data/Services/IRegistrationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Data.Services
+{
+    public interface IRegistrationService
+    {
+        ICourseService CourseService { get; }
+
+        ILearnerService LearnerService { get; }
+
+        void Save();
+
+    }
+}
