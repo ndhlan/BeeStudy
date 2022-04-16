ed433684342107044cefd98e509d2be9b0039718
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 14:04:07 -0600

    Scaffold Identity

---
+++ BeeStudy/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BeeStudy.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BeeStudy.Areas.Identity.IdentityHostingStartup))]
+namespace BeeStudy.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
