35b30acead04c543f0b7d7c48e034af4c7914455
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 12:52:46 -0600

    Update ApplicationDbContext and add initial migration

--- BeeStudy/Data/ApplicationDbContext.cs
+++ BeeStudy/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BeeStudy.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,28 @@ namespace BeeStudy.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Registration>().HasKey(re => new
+            {
+                re.CourseId,
+                re.LearnerId
+            });
+
+            modelBuilder.Entity<Registration>().HasOne(c => c.Course).WithMany(re => re.Registrations).HasForeignKey(c => c.CourseId);
+
+            modelBuilder.Entity<Registration>().HasOne(u => u.Learner).WithMany(re => re.Registrations).HasForeignKey(u => u.LearnerId);
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Learner> Learners { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
     }
+
+
 }
+
---
+++ BeeStudy/Data/Migrations/20220416184853_InitialMigration.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeeStudy.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UdemyId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Headline = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ListPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    LastUpdatedPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    LastUpdatedPriceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CurrentPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CurrentPriceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DiscountExpiration = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Browser = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Learners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsRegisteredUser = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Learners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    LearnerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => new { x.CourseId, x.LearnerId });
+                    table.ForeignKey(
+                        name: "FK_Registrations_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registrations_Learners_LearnerId",
+                        column: x => x.LearnerId,
+                        principalTable: "Learners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_LearnerId",
+                table: "Registrations",
+                column: "LearnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registrations");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Learners");
+        }
+    }
+}
