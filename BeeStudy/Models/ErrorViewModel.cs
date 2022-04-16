e9504ce4bfb056d4a5b3d17c2c964df18f5ba792
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-16 12:26:23 -0600

    Add project files.

---
+++ BeeStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BeeStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
