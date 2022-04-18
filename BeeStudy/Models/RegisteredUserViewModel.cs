85ca830f21b02e03367a78c198675d3231eabdc4
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-17 18:09:27 -0600

    Add RegisteredUserViewModel

---
+++ BeeStudy/Models/RegisteredUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Models
+{
+    public class RegisteredUserViewModel
+    {
+        public IdentityUser User { get; set; }
+
+        public Learner Learner { get; set; }
+    }
+}
