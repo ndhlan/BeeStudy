c804a8ddbd9c1442cd5c670a7aac6e725239f715
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-17 18:08:08 -0600

    Add IdentityUserService

---
+++ BeeStudy/Data/Services/IIdentityUserService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Data.Services
+{
+    public interface IIdentityUserService
+    {
+        IEnumerable<IdentityUser> GetAll();
+
+        IdentityUser GetById(string id);
+
+        IdentityUser GetByEmail(string email);
+
+        void Delete(string id);
+
+
+    }
+}
---
+++ BeeStudy/Data/Services/IdentityUserService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeStudy.Data.Services
+{
+    public class IdentityUserService : IIdentityUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IdentityUserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(string id)
+        {
+            var userToDelete = _context.Users.FirstOrDefault(u => u.Id.Equals(id));
+            _context.Users.Remove(userToDelete);
+            _context.SaveChanges();
+
+        }
+
+        public IEnumerable<IdentityUser> GetAll()
+        {
+            var result = _context.Users.ToList();
+
+            return result;
+        }
+
+        public IdentityUser GetByEmail(string email)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email.Equals(email));
+            return user;
+        }
+
+        public IdentityUser GetById(string id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id.Equals(id));
+            return user;
+
+        }
+
+    }
+}
