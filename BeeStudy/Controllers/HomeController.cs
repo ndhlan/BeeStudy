d4180a436a42fe153e89119e05db903a1bc5c0dc
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-18 17:41:01 -0600

    Add View Home/About

--- BeeStudy/Controllers/HomeController.cs
+++ BeeStudy/Controllers/HomeController.cs
@@ -18,15 +18,11 @@ namespace BeeStudy.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult About()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
