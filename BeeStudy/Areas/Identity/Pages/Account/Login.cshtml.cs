910768845da6f7195f8cd4d152026e5e9098fc64
Lan Nguyen <nguyen8083@saskpolytech.ca>
2022-04-19 15:39:12 -0600

    Add Check if user confirmed email upon login

--- BeeStudy/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BeeStudy/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -82,6 +82,15 @@ namespace BeeStudy.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+
+                //Check if the email was confirmed.
+                IdentityUser user = await _userManager.FindByNameAsync(Input.Email);
+                if (!await _userManager.IsEmailConfirmedAsync(user))
+                {
+                    ModelState.AddModelError("", "You need to confirm your email first.");
+                    return Page();
+                }
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -96,11 +105,15 @@ namespace BeeStudy.Areas.Identity.Pages.Account
                     _logger.LogWarning("User account locked out.");
                     return RedirectToPage("./Lockout");
                 }
+
                 else
                 {
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Page();
                 }
+
+
+
             }
 
             // If we got this far, something failed, redisplay form
